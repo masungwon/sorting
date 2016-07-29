49a32778e2d6695c0e48906614c494b0545604bb
Mackenzie Turner <turner.mackenzie.m@gmail.com>
2016-07-29 12:11:33 -0400

    added merge sort

--- mergesort.js
+++ mergesort.js
@@ -13,3 +13,46 @@ function split(wholeArray){
 	var secondHalf = wholeArray.slice(breakpointInd);
 	return [firstHalf, secondHalf];
 }
+
+function mergeSort(arr){
+	if(arr.length === 1) {
+		return arr;
+	}
+
+	// if(arr.length === 2){
+	// 	var halves = split(arr);
+	// 	var fist = halves[0];
+	// 	var last = halves[1];
+	// 	var output;
+		
+	// 	//first[0] > last[0] ? output = last.concat(first) : output = first.concat(last);
+	// 	return output;
+
+	 else {
+		var halves = split(arr);
+		var first = halves[0];
+		var last = halves[1];
+		first = mergeSort(first) 
+		last = mergeSort(last);
+		var output = [];
+		var limit = 0;
+		first.length > last.length ? limit = first.length : limit = last.length;
+		for(var i = 0; i < limit; i++){
+			if(first[i] && last[i])	{
+				if (first[i] > last[i]) {
+					output.push(last[i]);
+					output.push(first[i]);
+				} else {
+					output.push(first[i]);
+					output.push(last[i]);
+				}
+			} else if (first[i] === undefined){
+				output.push(last[i]);
+			} else if (last[i] === undefined){
+				output.push(first[i]);
+			}
+		}
+
+	}
+	return output;
+}
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -8,4 +8,17 @@ describe('Split Array function', function() {
 	it('throws an error for an empty array', function(){
 		expect( split( [1] )).toEqual('array has only one element');
  	});
-})
+});
+
+describe('mergest by sorting', function() {
+	it('is able to sort an array with two items', function(){
+		expect( mergeSort( [2,1] )).toEqual( [1,2] );
+	});
+	it('is able to sort an array with three items', function(){
+		expect( mergeSort( [3,1,2] )).toEqual( [1,2,3] );
+	});
+	it('is able to sort an array with MANY items', function(){
+		expect( mergeSort( [4,6,9,3,1,2,8] )).toEqual( [1,2,3,4,6,8,9] );
+ 	});
+});
+
